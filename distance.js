02df7ad1e8f9d6aa64e1435a25b2bd7380226473
howdy1227@naver.com <howdy1227@naver.com>
2023-06-25 15:32:29 +0900

    지도 거리 넣었으요

--- distance.js
+++ distance.js
@@ -1,9 +1,33 @@
-function getSelectedValues() {
-    var src = document.getElementById("src").value;
-    var dst = document.getElementById("dst").value;
 
-    if (src !== "" && dst!== "") {
-      var output = "첫 번째 건물: " + src + "<br>두 번째 건물: " + dst;
-      document.getElementById("output").innerHTML = output;
-    }
+  const reback = document.querySelector(".result");
+function loadInfo(e){
+  e.preventDefault();
+  const srcName = document.getElementById("src").value;
+  const dstName = document.getElementById("dst").value;
+  console.log(srcName);
+  console.log(dstName);
+  reback.style.display = "none";
+  if(srcName =="posco" && dstName =="Eltec"){
+    const resultMMS = document.querySelector("#poscoElte");
+    reback.style.display = "block";
+    resultMMS.style.display = "block";
+    
   }
+   if(srcName =="front" && dstName =="auditorium"){
+    const resultMMS = document.querySelector("#frontad");
+    reback.style.display = "block";
+    resultMMS.style.display = "block";
+   
+  }
+  else if(srcName =="subway" && dstName =="front"){
+    const resultMMS = document.querySelector("#subwayfront");
+    reback.style.display = "block";
+    resultMMS.style.display = "block";
+  }
+  else if(srcName =="front" && dstName =="posco"){
+    const resultMMS = document.querySelector("#frontposco");
+    reback.style.display = "block";
+    resultMMS.style.display = "block";
+  }
+}
+document.getElementById("input").addEventListener("submit", loadInfo);
